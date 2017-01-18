c1e3d1ad270be2105eb2328bdc9cf58f4a7b2aaf
MicicFilip <micic.filip@gmail.com>
2017-01-18 23:51:39 +0100

    inital commit ffs

---
+++ README.md
@@ -0,0 +1,10 @@
+# Report a Crime
+
+##Installation##
+Load database from dbDump, using mysqlworkbench and SQL Server. Set the connection string for database in web.config.
+For database: 
+1)username: root
+2)password: root
+
+Configure your server accordingly 
+
---
+++ Scripts/Script.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+    var menu = $('nav');
+    var origOffsetY = menu.offset().top;
+
+    function pushFooterDown() {
+        if ($('html').height() < $(window).height()) {
+            $('.footer').addClass('pushTOBottom');
+        }
+        else {
+            $('.footer').removeClass('pushTOBottom');
+        }
+    }
+
+    function scroll() {
+        if ($(window).scrollTop() >= origOffsetY) {
+            $('nav').addClass('navbar-fixed-top');
+            $('.container').addClass('container-padding');
+        } else {
+            $('nav').removeClass('navbar-fixed-top');
+            $('.container').removeClass('container-padding');
+        }
+
+
+    }
+
+
+    pushFooterDown();
+    document.onscroll = scroll;
+    window.onresize = pushFooterDown;
+});
+
+
